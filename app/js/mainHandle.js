bff08da1b276803c2e76bbcd627531bf4f6d3a0a
tinyFireFly <Remsfield.Papillon@student.fairfield.edu>
2021-05-15 15:28:49 -0400

    React Component setup

--- app/js/mainHandle.js
+++ app/js/mainHandle.js
@@ -1,6 +1,20 @@
 class welcomeHome extends React.Component{
-    constructor(props);
-    state = {
-        
+    constructor(props){
+        super(props);
+        state = {
+            location: '',
+            weather: ''
+        }  
+    }
+    
+
+    render() {
+        return(
+            <div>
+                <p>Hi! Is this working?</p>
+            </div>
+            )
     }
 }
+
+document.getElementById('mainCard').innerHTML = "Testing";
