aeb22fa96fef90e368f6a651a34ea854b15e6500
tinyFireFly <Remsfield.Papillon@student.fairfield.edu>
2021-05-16 10:55:10 -0400

    Yup! Got the hang of passing variables.

--- app/js/mainHandle.js
+++ app/js/mainHandle.js
@@ -6,20 +6,31 @@ const e = (<h1>
 );
 
 class WeatherCard extends React.Component {
-  constructor(props) {
+  constructor(props, temp, loc, name) {
     super(props);
-    this.state = { liked: false };
+    this.state = 
+    { 
+        yourName: name,
+        temperature: temp,
+        location: loc
+    };
   }
+  //A function to potentially change the picture of the weather, something like a state machine
 
   render() {
-    const j = (<h1>
-        Hi yall! I'm Remy and I'm using JS for the first time!
-    </h1>)
-    return (<h1>
-        Hi yall! I'm Remy and I'm using JS for the first time!
-    </h1>);
+    return (
+        <div>
+            <h1>
+                Hi y'all! I'm {this.state.yourName} and I'm in {this.state.location} for the first time and it's {this.state.temperature}!
+            </h1>
+        </div>
+    );
   }
 }
 
+// var firstName = prompt("What's your name?");
+// var yourHome = prompt("Where are you from?");
+var MyCard = new WeatherCard(null, "hot", "the Floss", "Remy");
+
 const domContainer = document.querySelector('#myDOM');
-ReactDOM.render(<WeatherCard />, domContainer);
+ReactDOM.render(MyCard.render(), domContainer);
