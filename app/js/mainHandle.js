e64f66c5c20bd3d529ead422fbb25f4a5acc6da4
tinyFireFly <Remsfield.Papillon@student.fairfield.edu>
2021-05-20 00:40:19 -0400

    k. made a react app to use dev server

--- app/js/mainHandle.js
+++ app/js/mainHandle.js
@@ -1,27 +1,65 @@
 'use strict';
 
-const e = (<h1>
-    It's working!!!!
-</h1>
-);
+//Make api call
+function fetchWeather(callback){
+    
+}
+
+function getWeatherData()
+{
+  var key = '';
+  fetch('https://api.openweathermap.org/data/2.5/weather?id=5128581&appid=69216bc1e255a60480a846fcb5004876')
+  .then(response => response.json())
+  .then((data) =>{
+      var weatherArray = data['weather'];
+      var mainData = weatherArray[0];
+      console.log(mainData['main']);
+      return String(mainData['main']);
+  })
+  .catch(err => alert("Error!"));
+}
 
 class WeatherCard extends React.Component {
-  constructor(props, temp, loc, name) {
+ 
+  //A function to potentially change the picture of the weather, something like a state machine
+  
+
+constructor(props, clim) {
     super(props);
     this.state = 
     { 
-        yourName: name,
-        temperature: temp,
-        location: loc
+        temperature: clim,
+        location: "loc"
     };
   }
-  //A function to potentially change the picture of the weather, something like a state machine
+  componentWillMount()
+  {
+    console.log("HIII");
+  }
+  getData()
+  {
+    fetch('https://api.openweathermap.org/data/2.5/weather?id=5128581&appid=69216bc1e255a60480a846fcb5004876')
+    .then(response => response.json())
+    .then((data) =>{
+        var weatherArray = data['weather'];
+        var mainData = weatherArray[0];
+        console.log(mainData['main']);
+        return String(mainData['main']);
+    })
+    .catch(err => alert("Error!"));
+  }
+  componentDidMount()
+  {
+    console.log("HIIII!");
+    this.setState({temperature: this.getWeatherData()});
+  }
 
   render() {
     return (
+        
         <div>
             <h1>
-                Hi y'all! I'm {this.state.yourName} and I'm in {this.state.location} for the first time and it's {this.state.temperature}!
+                Hi y'all! I'm Remy and I'm in {this.state.location} for the first time and it's {this.state.temperature}!
             </h1>
         </div>
     );
@@ -30,7 +68,8 @@ class WeatherCard extends React.Component {
 
 // var firstName = prompt("What's your name?");
 // var yourHome = prompt("Where are you from?");
-var MyCard = new WeatherCard(null, "hot", "the Floss", "Remy");
-
+var MyCard = new WeatherCard();
+var outsideForecast = getWeatherData();
 const domContainer = document.querySelector('#myDOM');
 ReactDOM.render(MyCard.render(), domContainer);
+
---
+++ react-app/src/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import './dist/style.css';
+
+function App() {
+  return (
+    <section class="splash">
+        <header>
+            <h1>Welcome</h1>
+            <p>Search bar goes here</p>
+        </header>
+        <div class="mainScriptContainer">
+            <div class="mainScriptUpperHalf">
+                <div class="upperHalfCardContainer" id="myDOM">
+                </div>
+                <div class="upperHalfSplitContainer">
+                    <div>
+                        <p>Placeholder Text</p>
+                    </div>
+                    <div>
+                        <p>Placeholder Text</p> 
+                    </div>
+                </div>
+            </div>
+            <div class="mainScriptLowerHalf">
+                <div>
+                    <p>Placeholder Text</p> 
+                </div>
+                <div>
+                    <p>Placeholder Text</p> 
+                </div>
+                <div>
+                    <p>Placeholder Text</p> 
+                </div>
+                <div>
+                    <p>Placeholder Text</p> 
+                </div>
+                <div>
+                    <p>Placeholder Text</p> 
+                </div>
+                <div>
+                    <p>Placeholder Tex</p> 
+                </div>
+            </div>
+        </div>
+    </section>
+  );
+}
+
+export default App;
